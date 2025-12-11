0f4730513c05e21fbddf2f6fe9f603ed2f278404
Julia Kang <julia7hk@gmail.com>
2025-12-11 11:28:47 -0500

    more main.py setup + .env update redirect uri

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)    # temp random key
+
+# in .env file
+# client_id
+# client_secret
+# redirect_uri
+
+scope = 'playlist-read-private'
+
+# multiple scopes
+# scope = 'playlist-read-private,streaming' 
 
 if __name__ == '__main__':
     app.run(debug=True)
